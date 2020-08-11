683e8e68baf6dd63ee2a049981638d08174d8978
jfilipe-dev <jfilipe1511@gmail.com>
2020-08-11 15:18:15 -0300

    Service Search Update

--- src/controllers/ServicesController.ts
+++ src/controllers/ServicesController.ts
@@ -64,48 +64,101 @@ export default class ServicesController {
   async index(request: Request, response: Response) {
     const {service, cost} = request.query;
 
-    if(!service || !cost) {
-      return response.status(400).json({
-        error: 'Missing filters to search services'
-      });
+    if(!service && !cost) {
+      const services = await db('services')
+      .join('users', 'services.user_id', '=', 'users.id')
+      .select(['services.*', 'users.*'])
+
+      response.json(services);
     }
 
-    const {min, max} = transformInScale(String(cost));
+    if (service && cost) {
+      const {min, max} = transformInScale(String(cost));
 
-    const services = await db('services')
-      .whereExists(function() {
-        if (min == 0 && max == 20) {
-          this.select('services.*')
-          .from('services')
-          .whereRaw('`services`.`user_id` = `users`.`id`')
-          .whereRaw('`services`.`cost` > ??', [Number(min)])
-          .whereRaw('`services`.`cost` <= ??', [Number(max)])
-        }
-        if (min == 20 && max == 50) {
-          this.select('services.*')
-          .from('services')
-          .whereRaw('`services`.`user_id` = `users`.`id`')
-          .whereRaw('`services`.`cost` > ??', [Number(min)])
-          .whereRaw('`services`.`cost` <= ??', [Number(max)])
-        }
-        if (min == 50 && max == 100) {
-          this.select('services.*')
-          .from('services')
-          .whereRaw('`services`.`user_id` = `users`.`id`')
-          .whereRaw('`services`.`cost` > ??', [Number(min)])
-          .whereRaw('`services`.`cost` <= ??', [Number(max)])
-        }
-        if (min == 100) {
-          this.select('services.*')
-          .from('services')
-          .whereRaw('`services`.`user_id` = `users`.`id`')
-          .whereRaw('`services`.`cost` > ??', [Number(min)])
-        }
-      })
+      const services = await db('services')
+        .whereExists(function() {
+          if (min == 0 && max == 20) {
+            this.select('services.*')
+            .from('services')
+            .whereRaw('`services`.`user_id` = `users`.`id`')
+            .whereRaw('`services`.`cost` > ??', [Number(min)])
+            .whereRaw('`services`.`cost` <= ??', [Number(max)])
+          }
+          if (min == 20 && max == 50) {
+            this.select('services.*')
+            .from('services')
+            .whereRaw('`services`.`user_id` = `users`.`id`')
+            .whereRaw('`services`.`cost` > ??', [Number(min)])
+            .whereRaw('`services`.`cost` <= ??', [Number(max)])
+          }
+          if (min == 50 && max == 100) {
+            this.select('services.*')
+            .from('services')
+            .whereRaw('`services`.`user_id` = `users`.`id`')
+            .whereRaw('`services`.`cost` > ??', [Number(min)])
+            .whereRaw('`services`.`cost` <= ??', [Number(max)])
+          }
+          if (min == 100) {
+            this.select('services.*')
+            .from('services')
+            .whereRaw('`services`.`user_id` = `users`.`id`')
+            .whereRaw('`services`.`cost` > ??', [Number(min)])
+          }
+        })
+        .where('services.service', '=', service as String)
+        .join('users', 'services.user_id', '=', 'users.id')
+        .select(['services.*', 'users.*'])
+
+      return response.json(services);
+    }
+
+    if (service && !cost) {
+      const services = await db('services')
       .where('services.service', '=', service as String)
       .join('users', 'services.user_id', '=', 'users.id')
       .select(['services.*', 'users.*'])
 
-    return response.json(services);
+      return response.json(services);
+    }
+
+    if (!service && cost) {
+        const {min, max} = transformInScale(String(cost));
+  
+        const services = await db('services')
+          .whereExists(function() {
+            if (min == 0 && max == 20) {
+              this.select('services.*')
+              .from('services')
+              .whereRaw('`services`.`user_id` = `users`.`id`')
+              .whereRaw('`services`.`cost` > ??', [Number(min)])
+              .whereRaw('`services`.`cost` <= ??', [Number(max)])
+            }
+            if (min == 20 && max == 50) {
+              this.select('services.*')
+              .from('services')
+              .whereRaw('`services`.`user_id` = `users`.`id`')
+              .whereRaw('`services`.`cost` > ??', [Number(min)])
+              .whereRaw('`services`.`cost` <= ??', [Number(max)])
+            }
+            if (min == 50 && max == 100) {
+              this.select('services.*')
+              .from('services')
+              .whereRaw('`services`.`user_id` = `users`.`id`')
+              .whereRaw('`services`.`cost` > ??', [Number(min)])
+              .whereRaw('`services`.`cost` <= ??', [Number(max)])
+            }
+            if (min == 100) {
+              this.select('services.*')
+              .from('services')
+              .whereRaw('`services`.`user_id` = `users`.`id`')
+              .whereRaw('`services`.`cost` > ??', [Number(min)])
+            }
+          })
+          .join('users', 'services.user_id', '=', 'users.id')
+          .select(['services.*', 'users.*'])
+  
+        return response.json(services);
+    }
+    
   }
 }
